2ab9fbb17900077dc81cd1381074beeb40359c27
Lucas Ribeiro <lucasnck@hotmail.com>
2021-06-28 15:16:46 -0300

    ✅ add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -73,8 +73,24 @@ function MyApp({ Component, pageProps }) {
           content="Lucas Ribeiro - JavaScript developer, specialist in e-commerce and frontend development, tap if you looking for freelancer development."
         />
         <meta property="og:image" content="/images/me.jpg" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KTWD6LF');`,
+          }}
+        ></script>
       </Head>
 
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTWD6LF"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
       <ThemeProvider theme={theme}>
         <Theme>
           <GlobalStyle />
